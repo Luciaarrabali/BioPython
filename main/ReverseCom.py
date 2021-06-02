12d67e0350043da993898b6f2509c439e38e5d39
carmenarrabali@gmail.com <carmenarrabali@gmail.com>
2021-06-02 19:21:27 +0200

    Reverse Complement

---
+++ main/ReverseCom.py
@@ -0,0 +1,12 @@
+# Autor: Carmen Lucía Arrabalí Cañete
+
+import DNA
+
+def reverse_complement(dna):
+    complement = {'A': 'T', 'T': 'A',
+                  'C': 'G', 'G': 'C'}
+    rc = ''
+    for base in dna:
+        rc += complement[base]
+    return ''.join(reversed(rc))
+print(DNA.dna, reverse_complement(DNA.dna))
