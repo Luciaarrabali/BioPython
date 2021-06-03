b49fe3a985d4fe8e1cb84579558a8ba1ecc8c5a1
carmenarrabali@gmail.com <carmenarrabali@gmail.com>
2021-06-03 13:10:08 +0200

    GC_Content Test

--- test/GC_ContentIT.py
+++ test/GC_ContentIT.py
@@ -1,9 +1,16 @@
 import unittest
 
+from main.GC_Content import gc_content
 
 class GC_ContentIT(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.gc_content = gc_content()
+
+    def test_GC_Content_first(self):
+        self.assertEqual(0.42, self.gc_content('TCGAACACTGGATGTTGATAAATTTGCTTTGCACCATCTGCCAGCATCAA'))
+
+    def test_GC_Content_second(self):
+        self.assertEqual(0.56, self.gc_content('GATGGTACCGTCAAACCTTCTGCACAGCGTGTCCGCCTCTTGAAGCAGGT'))
 
 
 if __name__ == '__main__':
