d660737ca0b1e6ca659ba35064fde977432e9851
carmenarrabali@gmail.com <carmenarrabali@gmail.com>
2021-06-03 11:10:38 +0200

    Updates

--- main/gc_content.py
+++ main/gc_content.py
@@ -2,22 +2,6 @@
 
 import DNA
 
-def gc_content1(dna):
-    gc = 0
-    for base in dna:
-        if base == 'C' or base == 'G':
-            gc += 1
-    return float(gc) / len(dna)
-
-print(gc_content1(DNA.dna))
-
-def gc_content2(dna):
-    gc = [base for base in dna if base in 'GC']
-    return float(len(gc))/len(dna)
-
-print(gc_content2(DNA.dna))
-
-def gc_content3(dna):
+def gc_content(dna):
     return float(dna.count('G') + dna.count('C'))/len(dna)
-
-print(gc_content3(DNA.dna))
+print(gc_content(DNA.dna))
