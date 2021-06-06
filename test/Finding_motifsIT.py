9eb1688fcb4d7365e8453e9f56849bcf7019d834
carmenarrabali@gmail.com <carmenarrabali@gmail.com>
2021-06-06 13:51:26 +0200

    Update with errors

--- test/Finding_motifsIT.py
+++ test/Finding_motifsIT.py
@@ -1,9 +1,17 @@
+# Autor: Carmen Lucía Arrabalí Cañete
+
 import unittest
 
+from main.Finding_motifs import motifs
+from collections import Counter
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+class Finding_motifsIT(unittest.TestCase):
+    def setUp(self):
+        self.FindingMotifs = motifs
+
+    def test_Finding_motifs_one(self):
+        self.assertEqual([('AG', 2), ('TT', 2)], motifs('TAGGTTTGAG'))
 
 
 if __name__ == '__main__':
